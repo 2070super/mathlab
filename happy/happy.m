201b126444dbe6272135954cb37d7368c527566b
admin <1025404072@qq.com>
2021-06-16 23:13:46 +0800

    happy

---
+++ happy/happy.m
@@ -0,0 +1,17 @@
+function happy 
+global x;
+x=input('«Î ‰»Î»À√˚','s');
+for i=0:3
+    if(i~=2)
+       first;
+    else 
+        x;
+           secound;
+    end
+end
+function secound
+global x;
+str=['happy birthday to',x];
+disp(str);
+function first
+disp('happy birthday to you');
